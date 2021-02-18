d26f24babb876159b8407ab69109330bfbf764dd
anetecima <a.cima@dynatech.lv>
2021-02-18 13:39:22 +0200

    null start state allowed without everything being desplayed as selected

--- src/components/DayCell/index.js
+++ src/components/DayCell/index.js
@@ -87,8 +87,7 @@ class DayCell extends Component {
     if (!preview) return null;
     const startDate = preview.startDate ? endOfDay(preview.startDate) : null;
     const endDate = preview.endDate ? startOfDay(preview.endDate) : null;
-    const isInRange =
-      (startDate && isAfter(day, startDate)) && (endDate && isBefore(day, endDate));
+    const isInRange = startDate && isAfter(day, startDate) && endDate && isBefore(day, endDate);
     const isStartEdge = !isInRange && isSameDay(day, startDate);
     const isEndEdge = !isInRange && isSameDay(day, endDate);
     return (
@@ -119,8 +118,7 @@ class DayCell extends Component {
       }
       startDate = startDate ? endOfDay(startDate) : null;
       endDate = endDate ? startOfDay(endDate) : null;
-      const isInRange =
-        (!startDate || isAfter(day, startDate)) && (!endDate || isBefore(day, endDate));
+      const isInRange = startDate && isAfter(day, startDate) && endDate && isBefore(day, endDate);
       const isStartEdge = !isInRange && isSameDay(day, startDate);
       const isEndEdge = !isInRange && isSameDay(day, endDate);
       if (isInRange || isStartEdge || isEndEdge) {
